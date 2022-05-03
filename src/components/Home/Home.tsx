757fcb7720f9ea5e7154d5b4fbf7661af79db5c7
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-05-03 13:16:15 +0530

    spline test

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { MainContainer } from "../../utils";
+import Spline from "@splinetool/react-spline";
 
 const Home = () => {
-  return <MainContainer>Hello, This is home!</MainContainer>;
+  return (
+    <MainContainer style={{ border: "2px solid white" }}>
+      <Spline scene="https://prod.spline.design/IrwsdsLYHRuhUWuo/scene.spline" />
+    </MainContainer>
+  );
 };
 
 export default Home;
