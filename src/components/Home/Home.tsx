2beba742995bd5f5636d03d963a3f465113a4488
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-05-08 23:23:57 +0530

    postcard updated

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { MainContainer } from "../../utils";
-import Spline from "@splinetool/react-spline";
+import PostCard from "../Card/PostCard";
+// import Spline from "@splinetool/react-spline";
 
 const Home = () => {
   return (
     <MainContainer>
+      <PostCard/>
       {/* <Spline scene="https://prod.spline.design/IrwsdsLYHRuhUWuo/scene.splin" /> */}
     </MainContainer>
   );
